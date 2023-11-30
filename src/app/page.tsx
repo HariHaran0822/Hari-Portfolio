17698789d4039f1b25974b4e23f4282a15ca5563
hariharan <hariharanengineer0120@gmail.com>
2023-11-30 12:26:57 +0530

    remove unwanter font style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,11 @@ import { ContactMe } from "@/Components/ContactMe";
 import { Projects } from "@/Components/Projects";
 import { NavBar } from "@/Components/nav";
 import React from "react";
-import Head from "next/head";
 import { Footer } from "@/Components/Footer";
 
 export default function Home() {
   return (
     <React.Fragment>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Caveat:wght@400;600&family=Ephesis&family=Shadows+Into+Light&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
       <main style={{ backgroundColor: "white", color: "black" }}>
         <NavBar />
         <HeroBanner />
